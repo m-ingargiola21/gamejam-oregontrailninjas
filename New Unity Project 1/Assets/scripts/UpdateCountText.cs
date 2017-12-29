1d8431b67b2bd68a9c746b9940f197069bba73f6
Jake Ross <charlesjross92@gmail.com>
2017-12-29 15:40:03 -0600

    Small modification to UI start scene

---
+++ New Unity Project 1/Assets/scripts/UpdateCountText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateCountText : MonoBehaviour {
+    Text playerCountText;
+    int players;
+	// Use this for initialization
+	void Start () {
+        playerCountText = GetComponent<Text>();
+        players = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerCountText.text = "Player Count: " + players.ToString();
+        int i = 1;
+        while (i < 5)
+        {
+            if (Input.GetButtonDown("Reload_P" + i.ToString()))
+            {
+                players++;
+                if (players > 4)
+                    players = 2;
+            }
+            i++;
+        }
+    }
+}
